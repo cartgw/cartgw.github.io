2bb39dc376ae06119e0152bb27a5b0230e9e6a8c
Ergaf <bymbarawko@gmail.com>
2020-03-22 21:35:33 +0300

    проверка на кнопку

--- js/index.js
+++ js/index.js
@@ -7,3 +7,15 @@ $('button').click(function(){
 $('.close-modal').click(function(){
     elements.removeClass('active');
 });
+
+
+$(".promofor").click(function () {
+    let lol = $('[name="promo"]')[0].value.toLowerCase();
+    if(lol != "skrypin"){
+        $(".promofor1").addClass("notpromo");
+    }
+    else{
+        $(".promofor1").removeClass("notpromo");
+        $(".modal-content form").submit();
+    }
+});
